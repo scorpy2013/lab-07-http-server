ecbb28ac595d505e1307b077e20f3a3b9db3a11a
scorpy2013 <61156928+scorpy2013@users.noreply.github.com>
2021-01-19 11:30:43 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -194,7 +194,6 @@ int Start(int argc, char* argv[]) {
   }
 }
 // Using: ./cmake-build-debug/tests 0.0.0.0 8080
-//int main(int argc, char* argv[]) 
-//{ 
+// int main(int argc, char* argv[]) { 
 //  return Start(argc, argv); 
 //}
